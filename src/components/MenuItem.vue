c94f1c81a3109d344e1f77ba18c687b559cd60f5
khemsoth <kevhemsoth@gmail.com>
2020-08-12 20:47:32 -0400

    funct: Menu items populate correctly

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="max-w-2xl ">
+    <ul v-for="item in items" v-bind:key="item.id">
+      <li>
+        <div  class="menu-item-title text-xl font-bold text-left">
+          {{ item.title }} 
+        </div>
+        <div class="m-3">
+          {{ item.desc }}
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuItem',
+  data() {
+    return {
+      items: [
+        {
+          title: 'Burger',
+          desc: 'Random burger description'
+        },
+                {
+          title: 'Hotdog',
+          desc: 'Random hotdog description'
+        },
+                {
+          title: 'Pasta',
+          desc: 'Random pasta description'
+        },
+                {
+          title: 'Sausage',
+          desc: 'Random sausage description'
+        },
+                {
+          title: 'Chicken',
+          desc: 'Random chicken description'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="menu">
+  <div class="menu flex flex-col items-center">
     <h1>This is an menu page</h1>
+    <MenuItem />
   </div>
 </template>
+
+<script>
+import MenuItem from '../components/MenuItem'
+
+export default {
+  name: 'Menu', 
+  components: {
+    MenuItem
+  },
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
