f81829dd45dd6617eda5409bc57b3e4e99333e6d
khemsoth <kevhemsoth@gmail.com>
2020-08-12 21:14:26 -0400

    funct: fixed menu list generation, style: added margins, shadow, position to menu, data: added price to menu items

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="max-w-2xl ">
-    <ul v-for="item in items" v-bind:key="item.id">
-      <li>
-        <div  class="menu-item-title text-xl font-bold text-left">
+  <div class="w-2/5 shadow-2xl p-4">
+    <ul>
+      <li     
+      v-for="item in items" 
+      v-bind:key="item.id"
+      class="mb-10"
+      >
+        <div class="menu-item-title text-xl font-bold text-left ml-12">
           {{ item.title }} 
+           <div class="float-right text-sm font-light italic mr-12">
+            {{ item.price }}
+          </div>
         </div>
         <div class="m-3">
           {{ item.desc }}
@@ -21,23 +28,28 @@ export default {
       items: [
         {
           title: 'Burger',
-          desc: 'Random burger description'
+          desc: 'Random burger description',
+          price: 13.99
         },
                 {
           title: 'Hotdog',
-          desc: 'Random hotdog description'
+          desc: 'Random hotdog description',
+          price: 5.99
         },
                 {
           title: 'Pasta',
-          desc: 'Random pasta description'
+          desc: 'Random pasta description',
+          price: 12.95
         },
                 {
           title: 'Sausage',
-          desc: 'Random sausage description'
+          desc: 'Random sausage description',
+          price: 10.99
         },
                 {
           title: 'Chicken',
-          desc: 'Random chicken description'
+          desc: 'Random chicken description',
+          price: 8.99
         },
       ]
     }
