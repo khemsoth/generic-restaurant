eaad1a78f73333677d079cbb68d97001812e2026
khemsoth <kevhemsoth@gmail.com>
2020-08-20 20:10:46 -0400

    layout: basic about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    This will be the about page
+    <h2 class="text-6xl">Who are we?</h2>
+    <div class="text-justify w-4/5 mx-auto">
+      <div class="my-5">
+        Non velit occaecat est amet amet sunt laborum elit eiusmod aliqua officia consequat. Veniam culpa tempor ullamco ullamco aliquip anim. Ut ex amet dolor incididunt occaecat id fugiat.
+      </div>
+      <div class="my-5">
+        Ipsum qui occaecat eiusmod ullamco et non minim aute aute excepteur mollit dolor ea mollit. Velit aute magna et deserunt magna nostrud anim eiusmod et laborum. Amet ipsum officia duis culpa enim veniam ipsum proident id.
+      </div>
+      <div class="my-5">
+        Lorem laboris fugiat quis nulla laboris ea qui eu do. Ea consectetur aliquip aute proident ipsum eiusmod ut enim velit in excepteur cupidatat esse. Do magna cillum magna commodo. Reprehenderit consequat officia occaecat laborum sint elit magna duis incididunt labore enim. Aliquip incididunt proident tempor sit in Lorem excepteur quis ullamco. Ad mollit quis exercitation ea proident enim consectetur excepteur qui adipisicing do consectetur reprehenderit et.
+      </div>
+    </div>
+    <div>
+      <img class="w-3/5 mx-auto my-10" src="../assets/priscilla-du-preez-W3SEyZODn8U-unsplash.jpg" alt="">
+    </div>
   </div>
 </template>
 
