4d0a7e636a2d4d1b314b07efd2a3b56f9f64669b
khemsoth <kevhemsoth@gmail.com>
2021-01-07 18:04:30 -0500

    added server.js server: added nodemailer

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const nodemailer = require('nodemailer')
+
+const PORT = process.env.PORT || 3000
+
+require('dotenv').config()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.post('/api/contact', function(req, res) {
+  var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    },
+  })
+  const mailOpts = {
+      to: process.env.EMAIL_USER,
+      from: process.env.EMAIL_USER,
+      subject: `${req.body.subject}`,
+      text: `
+        Name: ${req.body.name}
+        Email: ${req.body.email}
+        Phone: ${req.body.phone}
+        Message: ${req.body.message}
+      `
+    }
+  
+  transporter.sendMail(mailOpts, function(err, info) {
+    if(err) console.log(err)
+    else console.log(info)
+  })  
+})
+
+app.listen(PORT, function(req, res) {
+  console.log(`Server running on port ${PORT}`)
+})
