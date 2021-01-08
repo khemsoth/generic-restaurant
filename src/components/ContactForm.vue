84d64fff3e895fd8d40353fb91de88e97df75281
khemsoth <kevhemsoth@gmail.com>
2021-01-08 11:54:59 -0500

    contact form: added some responsive tags

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="flex flex-row justify-evenly mt-10 divide-x-2">
-    <div class="w-1/2">
-      <div class="my-12">
-        <h3 class="text-2xl font-bold">Address:</h3>
-        <p class="text-l">18415 Some Rd,<br> Avon Lake, OH, 44012  </p>
+  <div class="lg:flex lg:flex-row lg:justify-evenly lg:mt-10 lg:divide-x-2">
+    <div class="lg:w-1/2">
+      <div class="lg:my-12">
+        <h3 class="lg:text-2xl lg:font-bold">Address:</h3>
+        <p class="lg:text-l">18415 Some Rd,<br> Avon Lake, OH, 44012  </p>
       </div>
-     <div class="my-12">
-        <h3 class="text-2xl font-bold">Phone Number:</h3>
-        <p class="text-l">(123)456-7890</p>
+     <div class="lg:my-12">
+        <h3 class="lg:text-2xl lg:font-bold">Phone Number:</h3>
+        <p class="lg:text-l">(123)456-7890</p>
       </div>
-      <div class="my-12">
-        <h3 class="text-2xl font-bold">Hours:</h3>
-        <h4 class="text-xl font-medium">Monday - Thursday</h4>
-        <p class="text-l">11 AM - 11 PM</p>
-        <h4 class="text-xl font-medium">Friday & Saturday</h4>
-        <p class="text-l">11 AM - 1 AM</p>
-        <h4 class="text-xl font-medium">Sunday</h4>
-        <p class="text-l">12 PM - 8 PM</p>
+      <div class="lg:my-12">
+        <h3 class="lg:text-2xl lg:font-bold">Hours:</h3>
+        <h4 class="lg:text-xl lg:font-medium">Monday - Thursday</h4>
+        <p class="lg:text-l">11 AM - 11 PM</p>
+        <h4 class="lg:text-xl lg:font-medium">Friday & Saturday</h4>
+        <p class="lg:text-l">11 AM - 1 AM</p>
+        <h4 class="lg:text-xl lg:font-medium">Sunday</h4>
+        <p class="lg:text-l">12 PM - 8 PM</p>
       </div>
     </div>
 
-    <div class="w-1/2">
-      <form class="flex flex-col mx-auto">
-        <div class="my-4 w-4/5 mx-auto">
-          <label for="name" class="text-xl">Name</label><br>
-          <input class="border border-opacity-75 w-4/5" v-model="name" type="text" for="name" placeholder="John Smith">
+    <div class="lg:w-1/2">
+      <form class="lg:flex lg:flex-col lg:mx-auto">
+        <div class="lg:my-4 lg:w-4/5 lg:mx-auto">
+          <label for="name" class="lg:text-xl">Name</label><br>
+          <input class="lg:border lg:border-opacity-75 lg:w-4/5" v-model="name" type="text" for="name" placeholder="John Smith">
         </div>
         <div class="my-4 mx-auto w-4/5">
           <label for="email" class="text-xl">Email</label><br>
