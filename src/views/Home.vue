1c6a48e637af622dcf66b8c830076204dec63582
khemsoth <kevhemsoth@gmail.com>
2021-10-08 20:25:26 -0400

    Home: banner sizing and styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
 <div class="home bg-tertiary">
   <div class="banner bg-cover h-screen flex flex-col justify-center">
     <div class="flex justify-center">
-      <div class='flex flex-col items-center bg-white bg-opacity-75 w-1/2 lg:p-32 '>
+      <div class='flex flex-col items-center bg-white bg-opacity-75 w-4/5 py-20'>
         <h2 class="text-3xl text-bold text-primary">Lorem ipsum dolor sit amet.</h2>
         <p class="m-12 text-xl text-semi text-secondary">elit dolore malis elit summis amet quis magna quis summis fore quid eram sint
           ipsum quorum culpa quorum magna tamen. quem multos minim magna tamen eram aliqua anim magna veniam quis quis malis
@@ -40,5 +40,6 @@ export default {
 <style scoped>
   .banner {
     background-image: url('../assets/nick-karvounis-Ciqxn7FE4vE-unsplash.jpg');
+    height: 70vh;
   }
 </style>
